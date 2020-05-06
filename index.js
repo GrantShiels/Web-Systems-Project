2cccf3bc0503e080614237091a99b078e0e84b24
Grant Mac <grant.shiels@btinternet.com>
2020-05-06 16:30:32 +0100

    Added ejs module and express module

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-    fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080); 
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 8008
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
