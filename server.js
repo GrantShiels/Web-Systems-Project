61a570cec054363fc258058427722fe3683ed859
Grant Mac <grant.shiels@btinternet.com>
2020-05-10 23:40:01 +0100

    Edit to custom api

--- server.js
+++ server.js
@@ -9,35 +9,61 @@ const uri = "mongodb+srv://GShiels:Goldduck99@pokerumble-wpaen.mongodb.net/test?
 
 
 //set var for the cont for easy calls
-var app = express()
+var app = express();
 
+var mongoose = require("mongoose");
+var bodyParser = require('body-parser');
 
-
+//allows all requests to be sent to the console
+var morgan = require('morgan');
+app.use(morgan("dev"));
 
 //setting up the .ejs file paths for the url
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'Public')))
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+  next();
+});
+
+//connect mongoose to the DB
+mongoose.connect(uri, {useNewUrlParser: true});
+
+
+//Set the pages for the diffrent url
 app.set('views', path.join(__dirname, 'views'))
 app.set('scripts', path.join(__dirname, 'scripts'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 app.get('/pokedex', (req, res) => res.render('pages/pokedex'))
-app.get('/login', (req, res) => res.render('pages/login'))
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//app.get('/login', (req, res) => res.render('pages/login'))
 
-//app.listen(port)
 
-//login route
-app.route("/test")
-  .get(function(req, res){
-  res.send("This is the login form")
-})
-  .post(function(req, res){console.log("Porcessing")
-  res.send("processing the login form")
-})
+//-------------------------------------------
+//  Testing API Area
+//-------------------------------------------
 
-MongoClient.connect(uri, function (err, db) {
-  if(err) throw err;
+//instance of the router
+var apiRouter = express.Router();
 
-  console.log("end DB stuff")
-  
+//test to see if it's working
+apiRouter.get("/", function(req, res) {
+  res.json({message: "it's working"});
 });
+
+
+//the routes for the api will be here
+app.use("/api", apiRouter);
+
+//-------------------------------------------
+//
+//-------------------------------------------
+
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
